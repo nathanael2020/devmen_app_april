d3d4ff5a682d811bdd8121e6657bbaf02271e359
Pechnikov Maxim <parallel588@gmail.com>
2013-04-15 13:03:53 +0400

    added ingredients to the product page

--- spree_product_ingredients/app/models/spree/product_ingredient.rb
+++ spree_product_ingredients/app/models/spree/product_ingredient.rb
@@ -2,4 +2,10 @@ class Spree::ProductIngredient < ActiveRecord::Base
   attr_accessible :ingredient_id, :product_id, :amount_per_serving
   belongs_to :product
   belongs_to :ingredient
+
+  validates :product_id, :ingredient_id, :amount_per_serving, presence: true
+  delegate :name, :to => :ingredient, :prefix => true
+  def pretty_amount
+    "#{amount_per_serving} #{ingredient.unit}"
+  end
 end
