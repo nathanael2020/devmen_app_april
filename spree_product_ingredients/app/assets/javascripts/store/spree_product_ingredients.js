33ce576e60ac09fcc05065553cb31cd10dbdd5f6
Pechnikov Maxim <parallel588@gmail.com>
2013-04-18 10:29:02 +0400

    fixed search page

--- spree_product_ingredients/app/assets/javascripts/store/spree_product_ingredients.js
+++ spree_product_ingredients/app/assets/javascripts/store/spree_product_ingredients.js
@@ -2,6 +2,16 @@
 
 $(function(){
 
+  var amount_ingredient = function(list_ingredients, calculate_ingredient ){
+    var sum = 0;
+    $.each(list_ingredients, function(i, item){
+      var v = item.product_ingredient
+      if (v.ingredient_id == calculate_ingredient.id){
+        sum = sum + v.amount_per_serving;
+      }
+    })
+    return sum;
+  }
   $('#cart-toggle-ingredients-table').click(function(){
     $('#cart-ingredients').toggle();
     return false;
@@ -11,4 +21,66 @@ $(function(){
     $('#ingredients-table-container').load(window.location.href+'.js')
     return false;
   })
+
+  $("#ingredient_taxon").on("change", function(){
+    var current_taxon_id = $(this).val();
+    var table = ("table.table-search-result-ingredients:first")
+    var tr = ["<tr>"];
+    tr.push('<th>Ingredient</th>');
+    var product_ids = []
+    var products = $.map(supplment_products, function(i){
+      var taxon_ids = i.taxon_ids
+      if ($.inArray(parseInt(current_taxon_id), taxon_ids) != -1){
+        product_ids.push(i.id)
+        return i
+      }
+    })
+
+    $.each(products, function(i, item){
+      tr.push('<th>'+item.name+'</th>');
+    })
+    tr.push('</tr>');
+
+    var ingredients = $.map(ingredients_table, function(item){
+      var show_item = false
+      $.each(item[1], function(i, _item){
+        if ($.inArray(_item.product_id, product_ids ) != -1){
+          show_item = true;
+        }
+      });
+      if (show_item){
+        return [item]
+      }
+    });
+
+    $.each(ingredients, function(i, item){
+      tr.push('<tr>')
+
+      tr.push('<td>')
+      tr.push(item[0].ingredient.name)
+      tr.push('</td>')
+
+      $.each(products, function(i, _item){
+        var item_products = item[1]
+        var current_item_product =  null;
+        $.each(item_products,function(v, v_item){
+          if (v_item.product_id == _item.id){
+            current_item_product = v_item;
+          }
+        })
+        tr.push('<td>')
+        if (current_item_product){
+          tr.push(amount_ingredient(current_item_product.product_ingredients, item[0].ingredient))
+          tr.push(" "+item[0].ingredient.unit)
+        }
+        tr.push('</td>')
+      })
+
+      tr.push('</tr>')
+    });
+    $("tr, thead, tbody", table).remove();
+    $(table).append(tr.join(''))
+  });
+
+
 })
--- spree_product_ingredients/app/models/spree/product_decorator.rb
+++ spree_product_ingredients/app/models/spree/product_decorator.rb
@@ -7,8 +7,14 @@ Spree::Product.class_eval do
   end
   class << self
     def ingredients_table(products)
-      cell = Struct.new(:product, :ingredient, :product_id )
-      products.map{|j| j.product_ingredients.map{|v| cell.new(j, v.ingredient, j.id) } }.flatten.compact.group_by(&:ingredient)
+      cell = Struct.new(:product, :ingredient, :product_id, :taxon_ids, :product_ingredients )
+      products.map{|j|
+        j.product_ingredients.map{|v| cell.new(j, v.ingredient, j.id, j.taxon_ids, j.product_ingredients ) }
+      }.flatten.compact.group_by(&:ingredient)
     end
+  end
+  def as_json(*args)
+    obj = super(*args)
+    obj['product'].merge({:taxon_ids => self.taxon_ids})
   end
 end
